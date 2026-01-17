c6dbb8401c3e3a98af427a5f30d0702ece475a78
ElectronAmpere <learningthiruvalluvar@gmail.com>
2026-01-17 13:12:05 +0530

    added source documentation

--- lib/soilsensor/SoilSensor.cpp
+++ lib/soilsensor/SoilSensor.cpp
@@ -1,79 +1,157 @@
+/**
+ * @file SoilSensor.cpp
+ * @author VigneshRR
+ * @brief Implementation of the SoilSensor library.
+ * @version 0.2
+ * @date 2026-01-17
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
 #include "SoilSensor.h"
 
+// Initialize the static instance pointer to null. This is used by the static
+// pre/post-transmission callbacks to access the instance's members.
 SoilSensor* SoilSensor::_instance = nullptr;
 
+/**
+ * @brief See SoilSensor.h for documentation.
+ */
 SoilSensor::SoilSensor(ModbusMaster &node, uint8_t rePin, uint8_t dePin)
     : _node(node), _rePin(rePin), _dePin(dePin) {
+    // Set the static instance pointer to this object. This allows the static
+    // callback functions to access the pin numbers for RS485 direction control.
     _instance = this;
 }
 
+/**
+ * @brief See SoilSensor.h for documentation.
+ */
 void SoilSensor::begin(Stream &serial, long baud) {
     _serial = &serial;
-    _node.begin(1, *_serial); // Default slave ID 1
+    // Initialize the ModbusMaster instance with a default slave ID of 1.
+    // The actual slave ID of the sensor should be configured to match this,
+    // or changed using setDeviceAddress().
+    _node.begin(1, *_serial);
+    
+    // Configure the RS485 direction control pins as outputs.
     pinMode(_rePin, OUTPUT);
     pinMode(_dePin, OUTPUT);
+    
+    // Set the transceiver to receive mode by default.
     digitalWrite(_dePin, LOW);
     digitalWrite(_rePin, LOW);
+    
+    // Register the pre- and post-transmission callbacks with the ModbusMaster library.
+    // These functions will be called automatically to manage the RS485 transceiver's direction.
     _node.preTransmission(preTransmission);
     _node.postTransmission(postTransmission);
 }
 
+/**
+ * @brief See SoilSensor.h for documentation.
+ */
 void SoilSensor::preTransmission() {
     if (_instance) {
+        // Set the RS485 transceiver to transmit mode.
         digitalWrite(_instance->_rePin, HIGH);
         digitalWrite(_instance->_dePin, HIGH);
+        // A short delay is often required for the transceiver to switch modes.
         delay(10);
     }
 }
 
+/**
+ * @brief See SoilSensor.h for documentation.
+ */
 void SoilSensor::postTransmission() {
     if (_instance) {
-        _instance->_serial->flush(); // Wait for TX buffer to empty
+        // Ensure all data has been sent before switching back to receive mode.
+        _instance->_serial->flush();
+        // Set the RS485 transceiver back to receive mode.
         digitalWrite(_instance->_dePin, LOW);
         digitalWrite(_instance->_rePin, LOW);
     }
 }
 
+/**
+ * @brief See SoilSensor.h for documentation.
+ */
 uint16_t SoilSensor::getRegisterValue(uint16_t reg) {
+    // Perform a Modbus read holding registers transaction.
     uint8_t result = _node.readHoldingRegisters(reg, 1);
+    
+    // If the transaction is successful, return the value from the response buffer.
     if (result == _node.ku8MBSuccess) {
         return _node.getResponseBuffer(0);
     }
-    return 0xFFFF; // Error
+    
+    // Return an error value if the transaction failed.
+    return 0xFFFF;
 }
 
+/**
+ * @brief See SoilSensor.h for documentation.
+ */
 float SoilSensor::readMoisture() {
     uint16_t val = getRegisterValue(SOIL_MOISTURE_REG);
+    // Scale the raw value according to the sensor's documentation.
     return val != 0xFFFF ? val / 10.0f : -1.0f;
 }
 
+/**
+ * @brief See SoilSensor.h for documentation.
+ */
 float SoilSensor::readTemperature() {
     uint16_t val = getRegisterValue(SOIL_TEMPERATURE_REG);
+    // Scale the raw value according to the sensor's documentation.
     return val != 0xFFFF ? val / 10.0f : -999.0f;
 }
 
+/**
+ * @brief See SoilSensor.h for documentation.
+ */
 uint16_t SoilSensor::readConductivity() {
     return getRegisterValue(SOIL_CONDUCTIVITY_REG);
 }
 
+/**
+ * @brief See SoilSensor.h for documentation.
+ */
 float SoilSensor::readPH() {
     uint16_t val = getRegisterValue(SOIL_PH_REG);
+    // Scale the raw value according to the sensor's documentation.
     return val != 0xFFFF ? val / 10.0f : -1.0f;
 }
 
+/**
+ * @brief See SoilSensor.h for documentation.
+ */
 uint16_t SoilSensor::readNitrogen() {
     return getRegisterValue(SOIL_NITROGEN_REG);
 }
 
+/**
+ * @brief See SoilSensor.h for documentation.
+ */
 uint16_t SoilSensor::readPhosphorus() {
     return getRegisterValue(SOIL_PHOSPHORUS_REG);
 }
 
+/**
+ * @brief See SoilSensor.h for documentation.
+ */
 uint16_t SoilSensor::readPotassium() {
     return getRegisterValue(SOIL_POTASSIUM_REG);
 }
 
+/**
+ * @brief See SoilSensor.h for documentation.
+ */
 bool SoilSensor::readAll(SensorData &data) {
+    // The sensor's registers are not in a single contiguous block, so we must
+    // perform multiple read transactions.
+    
     uint8_t result = _node.readHoldingRegisters(SOIL_PH_REG, 1);
     if (result == _node.ku8MBSuccess) {
         data.ph = _node.getResponseBuffer(0) / 10.0f;
@@ -107,8 +185,14 @@ bool SoilSensor::readAll(SensorData &data) {
     return false;
 }
 
+/**
+ * @brief See SoilSensor.h for documentation.
+ */
 bool SoilSensor::setDeviceAddress(uint8_t newAddress) {
+    // Perform a Modbus write single register transaction to change the device address.
     uint8_t result = _node.writeSingleRegister(SOIL_DEVICE_ADDRESS_REG, newAddress);
+    
+    // If successful, re-initialize the ModbusMaster instance with the new address.
     if (result == _node.ku8MBSuccess) {
         _node.begin(newAddress, *_serial);
         return true;
@@ -116,7 +200,11 @@ bool SoilSensor::setDeviceAddress(uint8_t newAddress) {
     return false;
 }
 
+/**
+ * @brief See SoilSensor.h for documentation.
+ */
 bool SoilSensor::setBaudRate(uint16_t baudRateCode) {
+    // The sensor accepts specific codes for baud rate changes.
     // 1=2400, 2=4800, 3=9600
     return _node.writeSingleRegister(SOIL_BAUD_RATE_REG, baudRateCode) == _node.ku8MBSuccess;
 }
